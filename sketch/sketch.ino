f1466340d5fe9122b12bb9b7aadf989f6f2d4b73
deanzaka <deanzaka.h@gmail.com>
2015-06-07 00:09:26 +0700

    adding lcd code

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -1,12 +1,22 @@
+#include <LiquidCrystal.h>
+
+LiquidCrystal lcd(7, 6, 5, 4, 3, 2);
+
 void setup() {
   // initialize digital pin 13 as an output.
   pinMode(13, OUTPUT);
+  lcd.begin(16, 2);
+  lcd.print("hello, world!");
 }
 
 // the loop function runs over and over again forever
 void loop() {
   digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(100);              // wait for a second
+  delay(1000);              // wait for a second
+  lcd.setCursor(0, 1);
+  lcd.print(millis()/1000);
   digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
-  delay(100);              // wait for a second
+  delay(1000);              // wait for a second
+  lcd.setCursor(0, 1);
+  lcd.print(millis()/1000);
 }
